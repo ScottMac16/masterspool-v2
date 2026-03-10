7d4f4ede5d7d00e3d55b73895a34878e967291cf
ScottMac16 <sgjmcnamara@gmail.com>
2026-03-10 14:08:08 -0400

    styled sign-in and sign-up pages

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,11 @@
 import { ClerkProvider } from '@clerk/nextjs'
-import NavBar from '@/components/NavBar'
-import { headers } from 'next/headers'
 import './globals.css'
 
-export default async function RootLayout({ children }) {
-  const headersList = await headers()
-  const pathname = headersList.get('x-invoke-path') || ''
-  
-  const hideNav = pathname.startsWith('/sign-in') || pathname.startsWith('/sign-up')
-
+export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
         <body>
-          {!hideNav && <NavBar />}
           {children}
         </body>
       </html>
