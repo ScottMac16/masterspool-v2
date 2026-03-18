5026c5d1de6dc64fe07bb19a2f57eeecdedd9c71
ScottMac16 <sgjmcnamara@gmail.com>
2026-03-18 15:51:47 -0400

    fix tabsHidden pointer-events

--- app/(main)/leaderboard/page.jsx
+++ app/(main)/leaderboard/page.jsx
@@ -1,22 +1,37 @@
 'use client'
 
-import { useState } from 'react'
 import GolfLeaderboard from './GolfLeaderboard'
 import PoolLeaderboard from './PoolLeaderboard'
 import styles from './combined.module.css'
+import { useState, useRef } from 'react'
 
 export default function LeaderboardPage() {
   const [activePanel, setActivePanel] = useState(0)
   const [touchStart, setTouchStart] = useState(null)
   const [touchEnd, setTouchEnd] = useState(null)
+  const [tabsVisible, setTabsVisible] = useState(true)
+  const touchStartY = useRef(null)
+  const hideTimer = useRef(null)
 
   function onTouchStart(e) {
     setTouchStart(e.targetTouches[0].clientX)
+    touchStartY.current = e.targetTouches[0].clientY
     setTouchEnd(null)
   }
 
   function onTouchMove(e) {
     setTouchEnd(e.targetTouches[0].clientX)
+    const currentY = e.targetTouches[0].clientY
+    const diffY = touchStartY.current - currentY
+
+    if (diffY > 10) {
+      setTabsVisible(false)
+      if (hideTimer.current) clearTimeout(hideTimer.current)
+    } else if (diffY < -10) {
+      setTabsVisible(true)
+      if (hideTimer.current) clearTimeout(hideTimer.current)
+      hideTimer.current = setTimeout(() => setTabsVisible(false), 2000)
+    }
   }
 
   function onTouchEnd() {
@@ -48,12 +63,12 @@ export default function LeaderboardPage() {
     <div className={styles.container}>
 
       {/* Mobile tab switcher */}
-      <div className={styles.mobileTabs}>
+      <div className={`${styles.mobileTabs} ${tabsVisible ? styles.tabsVisible : styles.tabsHidden}`}>
         <button
           className={`${styles.mobileTab} ${activePanel === 0 ? styles.activeMobileTab : ''}`}
           onClick={() => setActivePanel(0)}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="30" height="30" viewBox="0 0 530 407.6" style={{enableBackground:'new 0 0 530 407.6'}}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 530 407.6">
             <path fill="currentColor" d="M256.2,312.1V39.1c0-4.8,3.9-8.8,8.8-8.8c4.9,0,8.8,4,8.8,8.8v273.1c0,4.8-4,8.8-8.8,8.8C260.2,320.9,256.2,317,256.2,312.1z"/>
             <path fill="currentColor" d="M469.2,312.1c0,19.9-22.7,36.8-65.6,48.8c-37.2,10.4-87.7,16.4-138.6,16.4c-50.9,0-101.4-6-138.6-16.4c-42.9-12-65.6-28.9-65.6-48.8c0-18.2,19.3-34.1,55.9-45.9c32-10.3,75.7-16.9,122.9-18.7l1.6-0.1v17.6l-1.4,0.1c-44.6,1.7-85.7,7.9-115.8,17.5c-27.1,8.7-43.3,19.7-43.3,29.5c0,10.8,19,22.7,50.7,31.8c35.6,10.2,83,15.8,133.6,15.8c50.5,0,98-5.6,133.5-15.8c31.8-9.1,50.8-21,50.8-31.8c0-9.8-16.2-20.8-43.3-29.5c-30.1-9.6-71.2-15.8-115.8-17.5l-1.4-0.1v-17.6l1.6,0.1c47.2,1.8,90.8,8.4,122.8,18.7C449.9,278,469.2,293.9,469.2,312.1z"/>
             <path fill="currentColor" d="M290.8,168.7l-2.4,1.9v-118l1.5,0c24,0.4,47.1,19.7,67.6,36.7l0.1,0.1c26,21.7,50.6,42.2,73,27.5l0.2-0.1c3.7-2.4,8.4-1.9,11.3,1.2c2.9,3.1,3.2,7.9,0.7,11.3c-31.2,41.5-63.7,36.1-92.4,31.3C328.7,157,309.9,153.8,290.8,168.7z"/>
@@ -63,7 +78,7 @@ export default function LeaderboardPage() {
           className={`${styles.mobileTab} ${activePanel === 1 ? styles.activeMobileTab : ''}`}
           onClick={() => setActivePanel(1)}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="30" height="30" viewBox="0 0 530 407.6" style={{enableBackground:'new 0 0 530 407.6'}}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 530 407.6">
             <path fill="currentColor" d="M503.1,48.6H27c-4.4,0-7.9,3.5-7.9,7.9v298.2c0,4.4,3.5,7.9,7.9,7.9h476.1c4.4,0,7.9-3.5,7.9-7.9V56.6C511,52.2,507.5,48.6,503.1,48.6L503.1,48.6z M34.9,152h460.2v89.5H34.9L34.9,152z M495.1,136.1H352.3l0-71.6h142.8L495.1,136.1z M336.4,136.1H193.6l0-71.6h142.8L336.4,136.1z M34.9,64.5h142.8v71.6H34.9L34.9,64.5z M495.1,346.8H34.9l0-89.5h460.2L495.1,346.8z"/>
             <path fill="currentColor" d="M224.6,167.8h-38.9c-4.4,0-7.9,3.5-7.9,7.9v38.9c0,4.4,3.5,7.9,7.9,7.9h38.9c4.4,0,7.9-3.5,7.9-7.9v-38.9C232.5,171.3,229,167.8,224.6,167.8z M216.6,206.7h-23v-23h23L216.6,206.7z"/>
             <path fill="currentColor" d="M300.8,167.8h-38.9c-4.4,0-7.9,3.5-7.9,7.9v38.9c0,4.4,3.5,7.9,7.9,7.9h38.9c4.4,0,7.9-3.5,7.9-7.9v-38.9C308.8,171.3,305.2,167.8,300.8,167.8z M292.9,206.7h-23v-23h23V206.7z"/>
@@ -91,7 +106,7 @@ export default function LeaderboardPage() {
         onMouseDown={onMouseDown}
         onMouseMove={onMouseMove}
         onMouseUp={onMouseUp}
-
+        onMouseLeave={onMouseUp}
       >
         <div className={styles.panel}>
           <GolfLeaderboard />
